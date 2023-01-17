f72ca6ce6cf5d0ad761ca34ed1092d9da652c05a
Jonathan Cañola Salazar <jocasa90@hotmail.com>
2023-01-17 16:14:08 -0500

    Creado el 15 de Septiembre para consumo de apirest apipets

---
+++ JS/concentrado.js
@@ -0,0 +1,17 @@
+let url = "http://localhost:4000/api/pets/concentrados";
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => mostrarData(data))
+  .catch((error) => console.log(error));
+const mostrarData = (data) => {
+  console.log(data);
+  let body = "";
+  for (let i = 0; i < data.length; i++) {
+    body += `<tr><td>${data[i].id}</td><td>${data[i].nombre}</td><td>${
+      data[i].stock
+    }</td><td>${data[i].precio}</td><td>${
+      data[i].stock > 0 ? "Disponible" : "Agotado"
+    }</td></tr>`;
+    document.getElementById("data").innerHTML = body;
+  }
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# _Front_ _APIPETS_
+Fron para consumo de apirest
+http://localhost:4000/api/pets/medicamentos
